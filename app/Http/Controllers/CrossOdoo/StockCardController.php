1ccb82242b123bb525b22f16ccd87970895ffb58
Mukhammad Fauzi <51779002+MFauziCollexe@users.noreply.github.com>
2026-08-04 12:47:07 +0700

    feat(stockcard): link approval request to stock picking and update vehicle plate number retrieval

--- app/Http/Controllers/CrossOdoo/StockCardController.php
+++ app/Http/Controllers/CrossOdoo/StockCardController.php
@@ -108,6 +108,9 @@ opening_balance AS (
 
     LEFT JOIN stock_picking sp
         ON sp.id = sml.picking_id
+    
+    LEFT JOIN approval_request ar
+        ON ar.id = sp.x_studio_approval_request_id
 
     LEFT JOIN stock_picking_type spt
         ON spt.id = sp.picking_type_id
@@ -183,7 +186,7 @@ SELECT
 
     wh.code                                       AS kd_gudang,
 
-    sp.x_studio_no_kendaraan                      AS no_mobil,
+    ar.x_studio_vehicle_plate_number              AS no_mobil,
 
     sp.name                                       AS no_reference_1,
 
@@ -218,6 +221,9 @@ JOIN stock_move sm
 
 LEFT JOIN stock_picking sp
     ON sp.id = sml.picking_id
+
+LEFT JOIN approval_request ar
+    ON ar.id = sp.x_studio_approval_id
 
 LEFT JOIN stock_picking_type spt
     ON spt.id = sp.picking_type_id
@@ -407,6 +413,9 @@ opening_balance AS (
     LEFT JOIN stock_picking sp
         ON sp.id = sml.picking_id
 
+    LEFT JOIN approval_request ar
+    ON ar.id = sp.x_studio_approval_id
+
     LEFT JOIN stock_picking_type spt
         ON spt.id = sp.picking_type_id
 
@@ -473,7 +482,7 @@ SELECT
     pt.name->>'en_US'                             AS nm_barang,
     sml.date::date                                AS tgl_tran,
     wh.code                                       AS kd_gudang,
-    sp.x_studio_no_kendaraan                      AS no_mobil,
+    ar.x_studio_vehicle_plate_number                      AS no_mobil,
     sp.name                                       AS no_reference_1,
     sp.origin                                     AS no_reference_2,
     COALESCE(sm.origin, sp.origin)                AS no_po_so,
@@ -500,6 +509,9 @@ JOIN stock_move sm
 
 LEFT JOIN stock_picking sp
     ON sp.id = sml.picking_id
+
+LEFT JOIN approval_request ar
+    ON ar.id = sp.x_studio_approval_id
 
 LEFT JOIN stock_picking_type spt
     ON spt.id = sp.picking_type_id
